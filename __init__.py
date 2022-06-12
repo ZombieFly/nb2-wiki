f8637525ff09a714cde8c3b5e67451b0490fd260
Zombie_fly <xyzomfly@gmail.com>
2022-06-12 19:21:33 +0800

    更正测试实例中代理设置问题

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 import mediawiki as wiki
 
-wiki.set_proxies({'http':'http:127.0.0.1:10809','https':'https://127.0.0.1:10809'})
+wiki.set_proxies({'http':'http://127.0.0.1:10809','https':'http://127.0.0.1:10809'})
 wiki.set_api_url('https://minecraft.fandom.com/zh/api.php')
 wiki.set_user_agent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36')
 print(wiki.summary('Java版1.7.2/开发版本', chars=200))
