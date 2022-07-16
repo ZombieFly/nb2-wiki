f2bb24401bcdb43563463d3f7d2096b23fd86b2c
ZombieFly <xyzomfly@gmail.com>
2022-07-16 16:54:03 +0800

    🌈 style: 整理代码

--- __init__.py
+++ __init__.py
@@ -50,7 +50,7 @@ search = on_command(cmd_start[0], aliases= set(cmd_start[1:]))
 ###################################################################
 
 def reply_out(msg_id:int, output:str) -> list[MessageSegment]:
-    """给消息包装上“回复“
+    """给消息包装上“回复”
 
     Args:
         msg_id (int): 所要回复的消息id
@@ -92,14 +92,15 @@ async def output(
     #! 此处curid_url无法正常赋值
     wiki.set_curid_url(mwiki.curid_url)
     wiki.set_user_agent(raw_MWiki.user_agent if not mwiki.user_agent else mwiki.user_agent)
-    wiki.set_proxies(PROXIES if mwiki.need_proxy else dict())
+    wiki.set_proxies(PROXIES if mwiki.need_proxy else {})
 
-    summ = await wiki.summary(title, auto_suggest= auto_suggest, redirect= redirect)
-    result = [title, summ[0], Handle(summ[1]).chars_max(max=200)]
+    curid, _summary = await wiki.summary(title, auto_suggest= auto_suggest, redirect= redirect)
+
+    _summary = Handle(Handle(_summary).chars_max(max=200)).nn_to_n()
     out = (
         (f'「{title}」\n' if has_title else '')
-        +f'{wiki.CURID_URL}{result[1]}\n'
-        +f'{result[2]}'
+        +f'{wiki.CURID_URL}{curid}\n'
+        +f'{_summary}'
     )
     return out if is_reply and not msg_id else reply_out(msg_id, out)
 
@@ -123,10 +124,10 @@ async def _search(bot: Bot, event: GroupMessageEvent, state: T_State, keywd= Com
         try:
             numb = int(numb)
             outstr = await output(title=state['results'][numb],
-                                            mwiki= (raw_MWiki if not state.__contains__('mwiki') else state['mwiki']),
-                                            msg_id= msg_id,
-                                            is_reply= True,
-                                            has_title= True)
+                                mwiki= (raw_MWiki if not state.__contains__('mwiki') else state['mwiki']),
+                                msg_id= msg_id,
+                                is_reply= True,
+                                has_title= True)
                   
             await search.send(outstr)
         except ValueError:
@@ -142,10 +143,10 @@ async def _search(bot: Bot, event: GroupMessageEvent, state: T_State, keywd= Com
         try:
             # * 有直接对应的页面
                 outstr = await output(title= keywd,
-                            mwiki= (raw_MWiki if not state.__contains__('mwiki') else state['mwiki']),
-                            redirect= True,
-                            msg_id= msg_id, 
-                            is_reply= True)
+                                    mwiki= (raw_MWiki if not state.__contains__('mwiki') else state['mwiki']),
+                                    redirect= True,
+                                    msg_id= msg_id, 
+                                    is_reply= True)
                 await search.finish(outstr)
         except wiki.exceptions.DisambiguationError as msg:
             # * 没有对应页面，但可生成相似结果列表
