577447ac2156589fe36ae93fb11a77148805ad02
ZombieFly <xyzomfly@gmail.com>
2022-07-20 21:21:37 +0800

    🌈 style: 依照PEP8整理代码

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ from nonebot.typing import T_State
 from nonebot.adapters.onebot.v11 import (
     GroupMessageEvent,
     MessageSegment,
-    GroupMessageEvent
+    MessageEvent
 )
 from nonebot.exception import RejectedException, FinishedException
 from nonebot.adapters.onebot.v11.permission import GROUP
@@ -27,14 +27,15 @@ RAW_MWIKI = config.RAW_MWIKI
 CMD_START = config.CMD_START
 
 
-cmd = on_command(CMD_START[0], aliases= set(CMD_START), permission=GROUP)
+cmd = on_command(CMD_START[0], aliases=set(CMD_START), permission=GROUP)
 
 CMD_START = [i+' ' for i in CMD_START]
-search = on_command(CMD_START[0], aliases= set(CMD_START[1:]))
+search = on_command(CMD_START[0], aliases=set(CMD_START[1:]))
 
 ###################################################################
 
-def reply_out(msg_id:int, output:str) -> list[MessageSegment]:
+
+def reply_out(msg_id: int, output: str) -> list[MessageSegment]:
     """给消息包装上“回复”
 
     Args:
@@ -46,22 +47,23 @@ def reply_out(msg_id:int, output:str) -> list[MessageSegment]:
     """
     return MessageSegment.reply(id_=msg_id) + MessageSegment.text(output)
 
+
 # TODO
 # ! 目前重定向可能会出现完全不相干的结果返回
 async def output(
     title: str,
-    mwiki: MWiki= RAW_MWIKI,
-    auto_suggest= True,
-    redirect= True,
-    is_reply= False,
-    has_title= False,
-    msg_id:int= None,
+    mwiki: MWiki = RAW_MWIKI,
+    auto_suggest=True,
+    redirect=True,
+    is_reply=False,
+    has_title=False,
+    msg_id: int = None,
 ) -> str:
     """将输入的标题转化为格式化的消息
 
     Args:
         title (str): 页面标题
-        mwiki (MWiki): MWik对象 
+        mwiki (MWiki): MWik对象
         auto_suggest (bool, optional): 是否启用自动建议 Defaults to True.
         redirect (bool, optional): 是否接受自动重定向 Defaults to True.
         is_reply (bool, optional): 所发送的消息是否“回复” Defaults to False.
@@ -71,36 +73,41 @@ async def output(
     Returns:
         str
     """
-    #大型赋值现场
+    # 大型赋值现场
     wiki.set_api_url(mwiki.api_url)
-    #todo
-    #! 此处curid_url无法正常赋值
     wiki.set_curid_url(mwiki.curid_url)
-    wiki.set_user_agent(RAW_MWIKI.user_agent if not mwiki.user_agent else mwiki.user_agent)
+    wiki.set_user_agent(
+        RAW_MWIKI.user_agent if not mwiki.user_agent else mwiki.user_agent
+    )
     wiki.set_proxies(PROXIES if mwiki.need_proxy else {})
 
-    curid, _summary = await wiki.summary(title, auto_suggest= auto_suggest, redirect= redirect)
+    curid, _summary = await wiki.summary(
+        title, auto_suggest=auto_suggest, redirect=redirect
+    )
 
     _summary = Handle(Handle(_summary).chars_max(max=200)).nn_to_n()
     out = (
         (f'「{title}」\n' if has_title else '')
-        +f'{wiki.get_curid_url()}{curid}\n'
-        +f'{_summary}'
+        + f'{wiki.get_curid_url()}{curid}\n'
+        + f'{_summary}'
     )
     return out if is_reply and not msg_id else reply_out(msg_id, out)
 
 ###################################################################
 
+
 @search.handle()
-async def _search(bot: Bot, event: GroupMessageEvent, state: T_State, keywd= CommandArg()):
+async def _search(
+    bot: Bot, event: MessageEvent, state: T_State, keywd=CommandArg()
+):
     try:
-        #直接调用搜索
+        # 子命令功能在个人会话中禁用，故此处实为个人会话直接调用搜索
         keywd = keywd.extract_plain_text()
-    except:
-        #通过Cmd子命令调用
+    except Exception:
+        # 通过Cmd子命令调用
         pass
 
-    numb:str = event.message.extract_plain_text()
+    numb: str = event.message.extract_plain_text()
     msg_id = event.message_id
     if numb and not keywd:
         # * 用户发送了对应条目的标号后的处理
@@ -108,18 +115,23 @@ async def _search(bot: Bot, event: GroupMessageEvent, state: T_State, keywd= Com
         await bot.delete_msg(message_id=state['refer_msg_id']['message_id'])
         try:
             numb = int(numb)
-            outstr = await output(title=state['results'][numb],
-                                mwiki= (RAW_MWIKI if not state.__contains__('mwiki') else state['mwiki']),
-                                msg_id= msg_id,
-                                is_reply= True,
-                                has_title= True)
-                  
+            outstr = await output(
+                title=state['results'][numb],
+                mwiki=(
+                    RAW_MWIKI
+                    if not state.__contains__('mwiki')else
+                    state['mwiki']
+                ),
+                msg_id=msg_id,
+                is_reply=True,
+                has_title=True
+            )
             await search.send(outstr)
         except ValueError:
-            #输入的非数字时的处理
+            # 输入的非数字时的处理
             await search.send('取消搜索')
         except IndexError:
-            #给的数大了
+            # 给的数大了
             await search.send(f'{numb}超出了索引')
         raise FinishedException
 
@@ -127,21 +139,30 @@ async def _search(bot: Bot, event: GroupMessageEvent, state: T_State, keywd= Com
         # * 会话开启的第一次处理
         try:
             # * 有直接对应的页面
-                outstr = await output(title= keywd,
-                                    mwiki= (RAW_MWIKI if not state.__contains__('mwiki') else state['mwiki']),
-                                    redirect= True,
-                                    msg_id= msg_id, 
-                                    is_reply= True)
-                await search.finish(outstr)
+            outstr = await output(
+                title=keywd,
+                mwiki=(
+                    RAW_MWIKI
+                    if not state.__contains__('mwiki') else
+                    state['mwiki']
+                ),
+                redirect=True,
+                msg_id=msg_id,
+                is_reply=True
+            )
+            await search.finish(outstr)
         except wiki.exceptions.DisambiguationError as msg:
             # * 没有对应页面，但可生成相似结果列表
             state['results'] = Handle(msg).refer_to_list(max=REFER_MAX)
-            out = ('有关结果如下，输入对应标号发起搜索，回复其他字符自动取消:\n'+
-                   '\n'.join(
+            out = (
+                '有关结果如下，输入对应标号发起搜索，回复其他字符自动取消:\n' +
+                '\n'.join(
                     f'[{n}]{state["results"][n]}'
                     for n in range(len(state["results"]))
-                    )
-                    +(f'\n(仅展示前{REFER_MAX}个结果)' if (len(state["results"]) > REFER_MAX) else '')
+                ) +
+                (f'\n(仅展示前{REFER_MAX}个结果)'
+                    if (len(state["results"]) > REFER_MAX) else
+                    '')
             )
             out = reply_out(msg_id=msg_id, output=out)
             state['refer_msg_id'] = await search.send(out)
@@ -150,42 +171,64 @@ async def _search(bot: Bot, event: GroupMessageEvent, state: T_State, keywd= Com
             # * 没有任何相关条目
             await search.finish(reply_out(output='没有找到任何相关结果', msg_id=msg_id))
 
+
 @cmd.handle()
-async def _cmd(bot:Bot, event: GroupMessageEvent,state: T_State, keywd= CommandArg()):
+async def _cmd(
+    bot: Bot, event: GroupMessageEvent, state: T_State, keywd=CommandArg()
+):
+    await bot.poke
     args = dict()
     args['group_id'] = event.group_id
     try:
         keywd = keywd.extract_plain_text()
     except AttributeError:
         await _search(bot, event, state, keywd)
-    else: 
+    else:
         if keywd[0] in get_driver().config.command_start:
-            #解析出子命令与参数
-            keywd = keywd[1:].split()
-            args['fn_args'] = keywd[1:]
+            # 解析出子命令与参数
+            _, *args['fn_args'] = keywd[1:].split()
             try:
-                #* 尝试执行子命令
+                # * 尝试执行子命令
                 try:
-                    #* 优先尝试普通权限命令
-                    await cmd.send(reply_out(event.message_id, await getattr(Cmd_member, keywd[0])(args)))
+                    # * 优先尝试普通权限命令
+                    await cmd.send(
+                        reply_out(
+                            event.message_id,
+                            await getattr(Cmd_member, keywd[0])(args)
+                        )
+                    )
                 except AttributeError:
                     raise AttributeError
-                except:
-                    #? 这里捕获了所有的异常，或许没法处理执行出现异常的平级命令
-                    #* 平级命令执行错误（大概率是没这个平级命令）
+                except Exception:
+                    # ? 这里捕获了所有的异常，或许没法处理执行出现异常的平级命令
+                    # * 平级命令执行错误（大概率是没这个平级命令）
                     if event.sender.role in ['admin', 'owner']:
-                        #* 管理员及群主会再尝试执行管理员级命令
-                        await cmd.send(reply_out(event.message_id, await getattr(Cmd_admin, keywd[0])(args)))
+                        # * 管理员及群主会再尝试执行管理员级命令
+                        await cmd.send(
+                            reply_out(
+                                event.message_id,
+                                await getattr(Cmd_admin, keywd[0])(args)
+                            )
+                        )
                     else:
-                        #* 普通成员调用不存在的平级命令或是管理员权限命令
-                        await cmd.finish(reply_out(event.message_id, '不存在的命令，或者您没有足够的权限执行'))
+                        # * 普通成员调用不存在的平级命令或是管理员权限命令
+                        await cmd.finish(
+                            reply_out(event.message_id, '不存在的命令，或者您没有足够的权限执行')
+                        )
             except AttributeError:
-                #* 不存在对应子命令时，调用seletc_wiki函数，获取可能的对应的wiki配置
-                #传入MWiki类
-                state['mwiki'] = await Cmd_member.select_mwiki(keywd[0], args['group_id'])
+                # * 不存在对应子命令时，调用seletc_wiki函数，获取可能的对应的wiki配置
+                # 传入MWiki类
+                state['mwiki'] = await Cmd_member.select_mwiki(
+                    keywd[0], args['group_id']
+                    )
                 if state['mwiki']:
                     await _search(bot, event, state, keywd)
                 else:
-                    await cmd.finish(reply_out(event.message_id, '不存在的命令或是已记录wiki，请检查是否具有对应权限或者输入是否正确'))
+                    await cmd.finish(
+                        reply_out(
+                            event.message_id,
+                            '不存在的命令或是已记录wiki，请检查是否具有对应权限或者输入是否正确'
+                            )
+                    )
             except Exception as err:
                 await cmd.finish(reply_out(event.message_id, str(err)))
--- handle.py
+++ handle.py
@@ -9,13 +9,13 @@ from . import mediawiki as wiki
 from .config import Config
 
 
-#* 文字处理部分
+# * 文字处理部分
 class Handle:
     '''
     用以处理简介、搜索结果输出
     '''
     def __init__(self, raw: str) -> None:
-        self.raw:str = raw
+        self.raw: str = raw
 
     def nn_to_n(self) -> str:
         '''
@@ -23,7 +23,7 @@ class Handle:
         '''
         return re.sub(r"\n\n", r"\n", self.raw)
 
-    def refer_to_list(self, max:int=0) -> list:
+    def refer_to_list(self, max: int = 0) -> list:
         '''
         将建议列表以列表形式输出,参数max控制返回列表最大长度
         '''
@@ -35,12 +35,17 @@ class Handle:
         控制输入文本最大字数,并在末尾追加省略信息
         '''
         txt = (self.raw if not txt else txt)
-        return  (txt if len(txt) <= max or not max else txt[:max] + f'……\n[字数大于{max}字部分被省略]')
+        return (
+            txt
+            if len(txt) <= max or not max else
+            txt[:max] + f'……\n[字数大于{max}字部分被省略]'
+        )
 
-#* 子命令
+
+# * 子命令
 class Cmd_member:
     """无权限限制命令
-    """    
+    """
     @classmethod
     async def list(cls, args):
         wiki_list = Data().get_wiki_list(args['group_id'])
@@ -56,8 +61,10 @@ class Cmd_member:
         return result
 
     @classmethod
-    async def select_mwiki(cls, wiki_name:str, group_id:int) -> Optional[MWiki]:
-        """ 
+    async def select_mwiki(
+        cls, wiki_name: str, group_id: int
+    ) -> Optional[MWiki]:
+        """
         获得已记录的MWiki对象
         不存在时返回None
 
@@ -67,17 +74,17 @@ class Cmd_member:
 
         Returns:
             MWiki | None
-        """        
+        """
         wiki_list = Data().get_wiki_list(group_id)
         if wiki_name in (
             wiki.name
             for wiki in wiki_list
                         ):
-            for wiki in wiki_list:
-                if wiki_name == wiki.name:
-                    return wiki
+            for _wiki in wiki_list:
+                if wiki_name == _wiki.name:
+                    return _wiki
         else:
-            #* 不存在对应wiki配置
+            # * 不存在对应wiki配置
             return None
 
     @classmethod
@@ -87,7 +94,7 @@ class Cmd_member:
 
 class Cmd_admin:
     """管理员权限命令
-    """    
+    """
     @classmethod
     async def add(cls, args):
         """#增加记录
@@ -103,14 +110,28 @@ class Cmd_admin:
             url = (fn_args[1] if fn_args[1][-1] == r'/' else fn_args[1]+r'/')
             if not (url[0:7] == 'https://' or url[0:6] == 'http://'):
                 url = 'https://' + url
-            mwiki = MWiki(name= fn_args[0],
-                        api_url= ((url+'api.php') if len(fn_args) == 2 else fn_args[1]),
-                        curid_url= ((url+'index.php?curid=') if len(fn_args) <= 2 else fn_args[2]),
-                        need_proxy=(False if len(fn_args) <=3 else bool(int(fn_args[3]))),
-                        user_agent=('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36'
-                            if len(fn_args) <= 4 else ''.join(fn_args[4:]))
-                        
-                    )
+            mwiki = MWiki(
+                name=fn_args[0],
+                api_url=((url+'api.php') if len(fn_args) == 2 else fn_args[1]),
+                curid_url=(
+                    (url+'index.php?curid=')
+                    if len(fn_args) <= 2 else
+                    fn_args[2]
+                ),
+                need_proxy=(
+                    False
+                    if len(fn_args) <= 3 else
+                    bool(int(fn_args[3]))
+                ),
+                user_agent=(
+                    r'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36\
+                        \(KHTML, like Gecko) Chrome/102.0.5005.63\
+                         Safari/537.36'
+                    if len(fn_args) <= 4 else
+                    ''.join(fn_args[4:])
+                )
+
+            )
         except Exception as err:
             return str(err)
         try:
@@ -121,8 +142,7 @@ class Cmd_admin:
         except Exception as err:
             return str(err)
         else:
-            Data().add_wiki(mwiki,args['group_id'])
-            
+            Data().add_wiki(mwiki, args['group_id'])
             return '记录完成'
 
     @classmethod
