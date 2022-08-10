76f4e9848f33f4c4f6f23f08a19533a9d7a35d13
ZombieFly <xyzomfly@gmail.com>
2022-08-10 19:03:44 +0800

    添加nb与onebot适配器依赖

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="nonebot-plugin-wiki",
-    version="0.0.4",
+    version="0.0.4-2",
     author="ZombieFly",
     author_email="xyzomfly@gmail.com",
     description="Nonebot2 插件，基于mediawiki api搜索条目",
@@ -13,7 +13,8 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/ZombieFly/nb2-wiki",
     packages=setuptools.find_packages(),
-    install_requires=["beautifulsoup4", "httpx"],
+    install_requires=["beautifulsoup4", "httpx",
+                      "nonebot2>=2.0.0b1", "nonebot-adapter-onebot"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
