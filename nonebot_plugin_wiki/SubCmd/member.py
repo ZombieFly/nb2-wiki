fac482d00570403f3187bcbf65dfa7c113bc3d0c
ZombieFly <xyzomfly@gmail.com>
2022-08-31 15:41:51 +0800

    🦄 refactor

--- nonebot_plugin_wiki/SubCmd/member.py
+++ nonebot_plugin_wiki/SubCmd/member.py
@@ -6,17 +6,10 @@ from ..data import Data
 
 
 async def ls(args: dict) -> str:
-    wiki_list = Data().get_wiki_list(args['group_id'])
-
-    if wiki_list:
-        result = "本群wiki记录列表如下：\n" + "\n".join(
-            f"[{wiki.name}]{wiki.api_url[:-8]}"  # type: ignore
-            for wiki in wiki_list
-        )
-    else:
-        result = "本群wiki记录为空"
-
-    return result
+    return ("本群wiki记录列表如下：\n"
+            + "\n".join(f"[{wiki.name}]{wiki.api_url[:-8]}"   # type: ignore
+                        for wiki in wiki_list)
+            ) if (wiki_list := Data().get_wiki_list(args['group_id'])) else "本群wiki记录为空"
 
 
 async def demo(args: dict) -> str:
